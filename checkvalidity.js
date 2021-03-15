c02ca50a100318813aa88d81916c4265337dcdda
Suraj Kumar pattanaik <44063901+surajpattanaik@users.noreply.github.com>
2021-03-15 13:25:28 +0530

    Add files via upload

---
+++ checkvalidity.js
@@ -0,0 +1,10 @@
+function validation()
+{
+var pass = document.getElementById("pass").value;
+if ((pass.length < 4) || (pass.length > 8))
+{
+alert("Your Password must be 4 to 8 Character");
+document.form.pass.select();
+return false;
+}
+}
