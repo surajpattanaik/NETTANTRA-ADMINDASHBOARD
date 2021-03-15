8c732a66ee5ec82cde8e04657ff63ea30195005d
Suraj Kumar pattanaik <44063901+surajpattanaik@users.noreply.github.com>
2021-03-15 23:24:59 +0530

    Delete checkvalidity.js

--- checkvalidity.js
+++
@@ -1,9 +0,0 @@
-function validation()
-{
-var pass = document.getElementById("pass").value;
-if ((pass.length < 4) || (pass.length > 8))
-{
-alert("Your Password must be 4 to 8 Character");
-return false;
-}
-}
